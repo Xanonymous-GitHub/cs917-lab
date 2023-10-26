92c3a9b4f5814762d7f9d3b1615861d079cbef30
Xanonymous <trusaidlin@gmail.com>
2023-10-26 03:25:15 +0100

    feat: 🎸 add error raising while time date is is illigal

--- programming_coursework/utils/timing.py
+++ programming_coursework/utils/timing.py
@@ -8,5 +8,11 @@ def date_str_to_utc_number(date_str: str) -> int:
     :return: utc number
     """
 
-    date_obj = datetime.strptime(date_str, "%d/%m/%Y")
+    try:
+        date_obj = datetime.strptime(date_str, "%d/%m/%Y")
+    except ValueError as e:
+        # FIXME: this is not a good practice. We should put the error message in the exception.
+        print("Error: invalid date value")
+        raise e
+
     return int(date_obj.replace(tzinfo=timezone.utc).timestamp())
